4afa3901bb71018ad0af278393bd1f52308ed351
Mohammed Ahazar <mdahazar.8675@gmail.com>
2025-05-30 10:08:44 +0530

    Add files via upload

---
+++ src/main/java/prj2/DBConnection.java
@@ -0,0 +1,20 @@
+package prj2;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/hostel_db", "root", "" // replace with your DB and password
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
